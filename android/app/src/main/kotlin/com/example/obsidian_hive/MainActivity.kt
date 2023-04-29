9c6d7ff0d605063a7bdbdcee9f10d2a2eaf03f85
stefanus <stefanus.ayudha@gmail.com>
2023-04-29 07:20:39 +0700

    init

---
+++ android/app/src/main/kotlin/com/example/obsidian_hive/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.obsidian_hive
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,84 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:flutter_markdown/flutter_markdown.dart';
+import 'package:url_launcher/url_launcher.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Obsidian Hive',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        useMaterial3: true,
+        primarySwatch: Colors.blue,
+      ),
+      home: const MyHomePage(title: 'Obsidian Hive'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  String _fileContent = "";
+
+  Future<String> loadAsset() async {
+    return await rootBundle.loadString('vault/README.md');
+  }
+
+  void linkTabCallback(
+    String text,
+    String? href,
+    String title,
+  ) {
+    _launchUrl(Uri.parse(href ?? ""));
+  }
+
+  Future<void> _launchUrl(Uri url) async {
+    if (!await launchUrl(url)) {
+      throw Exception('Could not launch $url');
+    }
+  }
+
+  @override
+  void initState() {
+    setState(() {
+      _fileContent = "Loading..";
+    });
+    loadAsset().then((value) {
+      setState(() {
+        _fileContent = value;
+      });
+    });
+
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: Markdown(
+        data: _fileContent,
+        selectable: true,
+        onTapLink: linkTabCallback,
+      ),
+    );
+  }
+}
